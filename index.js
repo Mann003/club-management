96bf1119d4e6de5b970ad943eb8ac18d9004a902
Mann Chhikara <75443469+Mann003@users.noreply.github.com>
2023-04-26 16:11:40 +0530

    .

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://https://iiitl-clubs-chat-server-production.up.railway.app/auth/google/callback"
+  callbackURL: "https://iiitl-clubs-chat-server-production.up.railway.app/auth/google/callback"
 },
   function (accessToken, refreshToken, profile, done) {
     userProfile = profile;
